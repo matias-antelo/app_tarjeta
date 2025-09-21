e73f44a80ab3f3d096b23f73b1413706ff2a42aa
nanontelo87 <anteloma87@gmail.com>
2025-09-21 10:30:24 -0300

    funcionalidad a botones

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btnInstagram = document.getElementById('btn-instagram');
+  const btnLinkedin = document.getElementById('btn-linkedin');
+  const btnGithub = document.getElementById('btn-github');
+
+  btnInstagram.addEventListener('click', () => {
+    window.open('https://www.instagram.com/nanoantelo/', '_blank');
+  });
+
+  btnLinkedin.addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/matias-antelo/', '_blank');
+  });
+
+  btnGithub.addEventListener('click', () => {
+    window.open('https://github.com/matias-antelo', '_blank');
+  });
+});
